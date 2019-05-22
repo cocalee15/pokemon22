fdc8d513d2348eb99bc8218d8a5dfb721fe38cd2
cocalee15 <oscar.steve11@outlook.com>
2019-05-22 12:37:18 -0600

    pokemon

---
+++ java/script.js
@@ -0,0 +1,14 @@
+var pokemon = angular.module("pokemon",[])
+pokemon .controller("pokedex",function($scope,$rootScope,$http) {
+
+ $http ({
+ 	method: "GET",
+ 	url:"https://pokeapi.co/api/v2/pokemon/25"
+
+
+ }) .then(function callbacksucces(y){ 
+ console.log(y);
+
+})
+ })
+
